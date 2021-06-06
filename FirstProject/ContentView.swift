22d79b02cf7ea2d0cf99d95c06e1f89677ef5c68
Mike Williams <diesel.kelly@gmail.com>
2021-06-06 15:49:01 -0400

    Initial Commit

---
+++ FirstProject/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  FirstProject
+//
+//  Created by Mike Williams on 6/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+                .padding()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Mike Williams on 6/5/21.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
